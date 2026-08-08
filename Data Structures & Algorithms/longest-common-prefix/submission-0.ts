5230ade06b694d365c9ae2c0632eb5fb25dfa8ac
Ravichandra <ravichandraraviraviravi@gmail.com>
2026-08-08 11:48:41 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs: string[]): string {
+        const arr=strs.sort((a,b)=>a.length-b.length)
+        let lcp=arr[0]
+        for(let i=1;i<arr.length;i++){
+            
+            if(arr[i].includes(lcp)){
+                continue;
+            }
+
+            while(!arr[i].includes(lcp)){
+                if(lcp.length==1){
+                    return ""
+                }
+                lcp=lcp.substring(0,lcp.length-1)
+            }
+        }
+        return lcp
+    }
+}
